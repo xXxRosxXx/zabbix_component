40ffa74626ae70339fa35d13b002ca5b2fcee367
xXxRosxXx <suttaweelukuan@gmail.com>
2021-09-11 15:22:48 +0700

    Delete DNS_time.py

--- DNS_time.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import dns.resolver
-import sys
-import time
-start = time.time()
-resolver = dns.resolver.Resolver()
-resolver.nameservers = [sys.argv[2]] # using google DNS
-result = resolver.query(sys.argv[1])
-print time.time()-start
